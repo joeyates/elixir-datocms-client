5ff0a068fe5cd0b1fcd01b79f9b9c1ffd5349055
Joe Yates <joe.g.yates@gmail.com>
2017-05-07 19:22:15 +0200

    Download all items

---
+++ lib/datocms/local.ex
@@ -0,0 +1,34 @@
+defmodule DatoCMS.Local do
+  @page_limit 500
+
+  def items do
+    DatoCMS.start()
+    fetch(1) |> handle([], 1, nil)
+  end
+
+  defp fetch(page) do
+    offset = @page_limit * (page - 1)
+    params = %{"page[limit]": @page_limit, "page[offset]": offset}
+    DatoCMS.Site.Item.index(params)
+  end
+
+  defp handle({:ok, response}, [], 1, nil) do
+    # First time
+    total_count = response["meta"]["total_count"]
+    total_pages = round(Float.ceil(total_count / @page_limit))
+    data = response["data"]
+    loop(data, 1, total_pages)
+  end
+  defp handle({:ok, response}, items, page, total_pages) do
+    data = response["data"]
+    loop(items ++ data, page, total_pages)
+  end
+
+  defp loop(items, total_pages, total_pages) do
+    # Done!
+    items
+  end
+  defp loop(items, page, total_pages) do
+    fetch(page + 1) |> handle(items, page + 1, total_pages)
+  end
+end
