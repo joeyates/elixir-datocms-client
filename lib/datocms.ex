e74fbd859b194f48d9aac511576b2a4986601c56
Joe Yates <joe.g.yates@gmail.com>
2017-06-25 15:55:50 +0200

    Add a minimal caching system

--- lib/datocms.ex
+++ lib/datocms.ex
@@ -1,6 +1,8 @@
 defmodule DatoCMS do
   use Application
 
+  @cache_path "tmp/dato.cache"
+
   def start(_start_type, _args \\ []) do
     DatoCMS.Supervisor.start_link()
   end
@@ -9,6 +11,25 @@ defmodule DatoCMS do
     {:ok, site} = DatoCMS.Site.fetch()
     {:ok, items} = DatoCMS.Items.fetch()
     {:ok, state} = DatoCMS.Internalizer.internalize(site, items)
-    DatoCMS.Repo.put(state)
+    {:ok} = put(state)
+  end
+
+  def put(state) do
+    {:ok} = DatoCMS.Repo.put(state)
+  end
+
+  def cache do
+    {:ok, state} = DatoCMS.Repo.all()
+    path = Path.dirname(@cache_path)
+    :ok = File.mkdir_p(path)
+    binary = :erlang.term_to_binary(state)
+    File.write!(@cache_path, binary)
+    {:ok}
+  end
+
+  def load_from_cache do
+    {:ok, binary} = File.read(@cache_path)
+    state = :erlang.binary_to_term(binary)
+    {:ok} = DatoCMS.Repo.put(state)
   end
 end
--- lib/datocms/repo.ex
+++ lib/datocms/repo.ex
@@ -9,13 +9,17 @@ defmodule DatoCMS.Repo do
     GenServer.call(:repo, {:put, state})
   end
 
-  def all(type) do
-    GenServer.call(:repo, {:all, type})
+  def all do
+    GenServer.call(:repo, {:all})
   end
 
-  def all!(type) do
-    {:ok, all} = all(type)
-    all
+  def items_of_type(type) do
+    GenServer.call(:repo, {:items_of_type, type})
+  end
+
+  def items_of_type!(type) do
+    {:ok, items_of_type} = items_of_type(type)
+    items_of_type
   end
 
   def get(specifier) do
@@ -30,8 +34,11 @@ defmodule DatoCMS.Repo do
   def handle_call({:put, state}, _from, _state) do
     {:reply, {:ok}, state}
   end
-  def handle_call({:all, type}, _from, state) do
-    items = state[:items_by_type][type]
+  def handle_call({:all}, _from, state) do
+    {:reply, {:ok, state}, state}
+  end
+  def handle_call({:items_of_type, type}, _from, state) do
+    items = state["items_by_type"][type]
     {:reply, {:ok, items}, state}
   end
   def handle_call({:get, specifier}, _from, state) do
@@ -40,23 +47,23 @@ defmodule DatoCMS.Repo do
 
   def handle_get({type, ids}, state) when is_list(ids) do
     {:ok, locale} = default_locale(state)
-    handle_get(state, {type, ids, locale})
+    handle_get({type, ids, locale}, state)
   end
   def handle_get({type, ids, locale}, state) when is_list(ids) do
-    items = state[:items_by_type][type]
-    requested = Enum.map(ids, fn (id) ->
+    items = state["items_by_type"][type]
+    localized_items = Enum.map(ids, fn (id) ->
       localize(items[id], locale)
     end)
-    {:ok, requested}
+    {:reply, {:ok, localized_items}, state}
   end
   def handle_get({type, id}, state) do
     {:ok, locale} = default_locale(state)
-    handle_get(state, {type, id, locale})
+    handle_get({type, id, locale}, state)
   end
   def handle_get({type, id, locale}, state) do
-    items = state[:items_by_type][type]
+    items = state["items_by_type"][type]
     item = localize(items[id], locale)
-    {:ok, item}
+    {:reply, {:ok, item}, state}
   end
 
   defp default_locale(state) do
