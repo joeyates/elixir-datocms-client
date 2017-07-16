d98763c91ae847a5a80c8dda74f47963ad3cf8ab
Joe Yates <joe.g.yates@gmail.com>
2017-07-16 21:26:18 +0200

    Generate title meta tags

---
+++ lib/datocms/meta_tag/helpers.ex
@@ -0,0 +1,21 @@
+defmodule DatoCMS.MetaTag.Helpers do
+  def tag(tag_name, attributes) do
+    %{tag_name: tag_name, attributes: attributes}
+  end
+
+  def meta_tag(name, content) do
+    tag("meta", %{name: name, content: content})
+  end
+
+  def og_tag(property, content) do
+    tag("meta", %{property: property, content: content})
+  end
+
+  def card_tag(name, content) do
+    meta_tag(name, content)
+  end
+
+  def content_tag(tag_name, content) do
+    %{tag_name: tag_name, content: content}
+  end
+end
