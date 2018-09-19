cfb44c3c7c08f487080a8b6d224c1617fbc7ba26
Joe Yates <joe.g.yates@gmail.com>
2018-09-19 13:05:48 +0200

    Remove superfluous quotes

--- lib/datocms/site.ex
+++ lib/datocms/site.ex
@@ -2,7 +2,7 @@ defmodule DatoCMS.Site do
   require DatoCMS.Client
 
   def fetch() do
-    params = %{"include": "item_types,item_types.fields"}
+    params = %{include: "item_types,item_types.fields"}
     DatoCMS.Client.Site.get(params)
   end
 end
